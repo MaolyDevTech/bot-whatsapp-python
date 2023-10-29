d78d27ed4343abeafb2a39c0f628eb18d0980ddb
MaolyDevTech <144358009+MaolyDevTech@users.noreply.github.com>
2023-10-29 19:05:36 +0000

    refactor

--- zapbot.py
+++ zapbot.py
@@ -4,24 +4,27 @@ import time
 class WhatsappBot:
     def __init__(self):
         self.mensagem = "Olá! 🌟 Bem-vindo a novembro! 🍂 Este é o Zap Bot, seu assistente automatizado. Vamos tornar este mês incrível! 🚀, Aqui Maoly treinando automação"
-        self.grupos = ["Comunidade Joga Junto - Chat geral", "Alfert Garcia Edensparck", "Samuel Lara"]
+        self.grupos = ["Grupo1", "Grupo2", "Grupo3"]
         options = webdriver.ChromeOptions()
         options.add_argument('lang=pt-br')
         self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe', chrome_options=options)
+        
 
     def EnviarMensagens(self):
-        self.driver.get('https://web.whatsapp.com/')
+        # Aguarde o usuário escanear o código QR manualmente
+        #input("Pressione Enter após escanear o código QR...")
+        self.driver.get('https://web.whatsapp.com')
         time.sleep(30)
+
         for grupo in self.grupos:
             grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
-            time.sleep(3)
             grupo.click()
-            chat_box = self.driver.find_element_by_class_name('_1VZX7')
             time.sleep(3)
+            chat_box = self.driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="6"]')
             chat_box.click()
+            time.sleep(3)
             chat_box.send_keys(self.mensagem)
             botao_enviar = self.driver.find_element_by_xpath("//span[@data-icon='send']")
-            time.sleep(3)
             botao_enviar.click()
             time.sleep(5)
 
